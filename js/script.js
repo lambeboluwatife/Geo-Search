cc56c231a0ccaea1e782109605909f4e2ea1f10d
Lambe Boluwatife <40843759+lambeboluwatife@users.noreply.github.com>
2019-11-27 23:54:14 +0100

    Revert "merging with develop"

--- js/script.js
+++ js/script.js
@@ -55,45 +55,88 @@ function initMap() {
         });
 
         // Output 1
-        let iconCode = data.weather[0].icon;
+        let iconCode = data.weather[0].icon
+        output1 += `
+        <style>
+        .result {
+          min-width: 250px;
+          margin-bottom: 30px;
+        }
+        .weather-city {
+          font: Arial black;;
+          margin-top: 0;
+        }
+        img {
+          height: 80px;
+          width: 80px;
+          border-style: none;
+        }
+        h4 {
+          margin-top: 5px;
+          margin-bottom: 5px;
+          display: block;
+        }
+        </style>
+        <div class='result'>
+        <center>
+          <h2 class="weather-city">Weather in ${input.value}</h2>
+          <img src="http://openweathermap.org/img/w/${iconCode}.png">
+          <h4>${temp}℃</h4>
+          <p>${data.weather[0].description}</p>
+        </center>
+        </div>
+        `
+        weatherOutput1.innerHTML = output1;
 
-        weatherOutput1.style.border =  '1px solid #7451eb';
-        weatherOutput1.style.borderRadius = '5px';
-
-        document.querySelector('.inputValue').innerHTML = `Weather in ${input.value}`;
-        document.querySelector('.iconCode').innerHTML = `<img src="http://openweathermap.org/img/w/${iconCode}.png">`;
-        document.querySelector('.description').innerHTML = `${data.weather[0].description}`
-        document.querySelector('.temp1').innerHTML = `${Math.round(temp)}℃`;
-
-        document.querySelector('.desc').innerHTML = `Description &#8594; ${data.weather[0].description}`;
-        document.querySelector('.desc').style.border = '1px solid #7451eb';
-      	document.querySelector('.temp').innerHTML = `Temperature &#8594; ${Math.round(temp)}℃`;
-        document.querySelector('.temp').style.border = '1px solid #7451eb';
-      	document.querySelector('.speed').innerHTML = `Wind Speed &#8594; ${data.wind.speed}m/s`;
-        document.querySelector('.speed').style.border = '1px solid #7451eb';
-        document.querySelector('.humidity').innerHTML = `Humidity &#8594; ${data.main.humidity}%`;
-        document.querySelector('.humidity').style.border = '1px solid #7451eb';
-      	document.querySelector('.press').innerHTML = `Pressure &#8594; ${data.main.pressure} hpa`;
-        document.querySelector('.press').style.border = '1px solid #7451eb';
-      	document.querySelector('.coord').innerHTML = `Geo Coord &#8594; [${lat}, ${lon}]`;
-        document.querySelector('.coord').style.border = '1px solid #7451eb';
-
-        // Temperature Converter
-        document.querySelector('.celcius').addEventListener('click', convertToCelcius);
-        document.querySelector('.fahranheit').addEventListener('click', convertToFahranheit);
-
-        function convertToCelcius() {
-          var celciusTemp = temp;
-          document.querySelector('.temp').innerHTML = Math.round(temp) + '&deg' + 'C';
-          document.querySelector('.temp1').innerHTML = Math.round(temp) + '&deg' + 'C';
+        // Output 2
+        output2 += `
+        <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            border: 1px solid #ddd;
+            text-align: left;
         }
 
-        function convertToFahranheit() {
-          var fahranheitTemp = temp;
-          var fahranheitTemperature = (temp * 9/5) + 32;
-          document.querySelector('.temp').innerHTML = Math.round(fahranheitTemperature) + '&deg' + 'F';
-          document.querySelector('.temp1').innerHTML = Math.round(fahranheitTemperature) + '&deg' + 'F';
+        th, td {
+            text-align: left;
+            padding: 15px;
+            border: 1px solid #ddd;
+            text-align: left;
+            font-family: Cambria;
         }
+
+        tr:nth-child(even){background-color: #f2f2f2}
+        </style>
+        <table style="width:100%">
+          <tr>
+            <td>Description</td>
+            <td>${data.weather[0].description}</td>
+          </tr>
+          <tr>
+            <td>Wind Speed</td>
+            <td>${data.wind.speed} m/s</td>
+          </tr>
+          <tr>
+            <td>Temperature</td>
+            <td>${temp}℃</td>
+          </tr>
+          <tr>
+            <td>Humidity</td>
+            <td>${data.main.humidity}%</td>
+          </tr>
+          <tr>
+            <td>Pressure</td>
+            <td>${data.main.pressure} hpa</td>
+          </tr>
+          <tr>
+            <td>Geo Coords</td>
+            <td>[${lat}, ${lon}]</td>
+          </tr>
+        </table>
+        `
+        weatherOutput2.innerHTML = output2;
+
       })
       .catch(function(error) {
         // catch any errors
